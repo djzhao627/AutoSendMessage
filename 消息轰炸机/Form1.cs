c58f1ce2a4585d234137c7bf6f446ad6367431bd
Chenng <email@zhaoce.name>
2016-08-13 17:54:21 +0800

    Signed-off-by: Chenng <email@zhaoce.name>

--- 消息轰炸机/Form1.cs
+++ 消息轰炸机/Form1.cs
@@ -14,20 +14,22 @@ using System.IO;
 
 namespace 消息轰炸机 {
     public partial class Form1 : Form {
+
         [DllImport("user32.dll")]
         private static extern int SendMessageA(IntPtr hWnd, int Msg, int wParam, int lParam);
+
         private const int WM_CHAR = 0X102;
+
         [DllImport("user32")]
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint control, Keys vk);
+
         //解除注册热键的api
         [DllImport("user32")]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-        [DllImport("user32", EntryPoint = "PaintDesktop")]
-        public static extern int PaintDesktop(int hdc);
-        [DllImport("user32.dll", EntryPoint = "GetCursor")]
-        public static extern IntPtr GetCursor();
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         public static extern IntPtr GetForegroundWindow();
+
         public Form1() {
             InitializeComponent();
         }
@@ -50,6 +52,7 @@ namespace 消息轰炸机 {
                 SendMessageA(myIntPtr, WM_CHAR, ch[i], 0);
             }
         }
+
         static void write() {
             while (true) {
                 StreamReader sr = new StreamReader("library.txt", Encoding.UTF8);
@@ -58,16 +61,11 @@ namespace 消息轰炸机 {
                     InputStr(GetForegroundWindow(), line);
                     SendKeys.SendWait("{enter}");
                 }
-
-                //string[] lib = { "消息1", "消息2" };
-                //for (int i = 0; i < 2; i++) {
-                //    InputStr(GetForegroundWindow(), lib[i]);
-                //    SendKeys.SendWait("{enter}");
-                //}
             }
-
         }
+
         Thread t = new Thread(write);
+
         protected override void WndProc(ref Message m) {
             switch (m.Msg) {
                 case 0x0312:  //这个是window消息定义的注册的热键消息  
@@ -77,16 +75,12 @@ namespace 消息轰炸机 {
                             t.Resume();
                         }
                         catch {
-                            try { t.Start(); }
-                            catch { }
+                            try { t.Start(); } catch { }
                         }
                     }
                     else if (m.WParam.ToString() == "456") {
                         //停止
-                        try {
-                            t.Suspend();
-                        }
-                        catch { }
+                        try { t.Suspend(); } catch { }
                     }
                     break;
             }
